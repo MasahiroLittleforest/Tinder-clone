439366de066a7009f30891951a35babbd89d35ec
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-06-20 00:55:13 +0900

    finished lesson 3

---
+++ SwipeMatchFirestore/Views/TopNavigationStackView.swift
@@ -0,0 +1,39 @@
+//
+//  TopNavigationStackView.swift
+//  SwipeMatchFirestore
+//
+//  Created by 小森 将大 on 2019/06/20.
+//  Copyright © 2019 小森 将大. All rights reserved.
+//
+
+import UIKit
+
+class TopNavigationStackView: UIStackView {
+
+    let settingButton = UIButton(type: .system)
+    let messageButton = UIButton(type: .system)
+    let fireImageView = UIImageView(image: #imageLiteral(resourceName: "app_icon"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        heightAnchor.constraint(equalToConstant: 80).isActive = true
+        fireImageView.contentMode = .scaleAspectFit
+        
+        settingButton.setImage(#imageLiteral(resourceName: "top_left_profile").withRenderingMode(.alwaysOriginal), for: .normal)
+        messageButton.setImage(#imageLiteral(resourceName: "top_right_messages").withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        [settingButton, UIView(), fireImageView, UIView(), messageButton].forEach { (v) in
+            addArrangedSubview(v)
+        }
+        
+        distribution = .equalCentering
+        
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins = .init(top: 0, left: 16, bottom: 0, right: 16)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
