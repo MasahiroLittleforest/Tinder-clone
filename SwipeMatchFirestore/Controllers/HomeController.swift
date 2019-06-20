8c0202cb18052f5f870b90232e1957ad9bb2fe34
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-06-20 22:47:00 +0900

    finished lesson 7

--- SwipeMatchFirestore/Controllers/HomeController.swift
+++ SwipeMatchFirestore/Controllers/HomeController.swift
@@ -19,6 +19,11 @@ class HomeController: UIViewController {
         User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
     ]
     
+    let cardViewModels = [
+        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c").toCardViewModel(),
+        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c").toCardViewModel()
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
@@ -26,20 +31,29 @@ class HomeController: UIViewController {
     }
     
     fileprivate func setupDummyCards() {
-        users.forEach { (user) in
+        cardViewModels.forEach { (cardViewModel) in
             let cardView = CardView(frame: .zero)
-            cardView.imageView.image = UIImage(named: user.imageName)
-            cardView.informationLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
-            
-            let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
-            attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
-            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
-            
-            cardView.informationLabel.attributedText = attributedText
+            cardView.imageView.image = UIImage(named: cardViewModel.imageName)
+            cardView.informationLabel.attributedText = cardViewModel.attributedString
+            cardView.informationLabel.textAlignment = cardViewModel.textAlignment
             
             cardsDeckView.addSubview(cardView)
             cardView.fillSuperview()
         }
+//        users.forEach { (user) in
+//            let cardView = CardView(frame: .zero)
+//            cardView.imageView.image = UIImage(named: user.imageName)
+//            cardView.informationLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
+//
+//            let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
+//            attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
+//            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
+//
+//            cardView.informationLabel.attributedText = attributedText
+//
+//            cardsDeckView.addSubview(cardView)
+//            cardView.fillSuperview()
+//        }
     }
 
     // MARK:- Fileprivate
---
+++ SwipeMatchFirestore/ViewModels/CardViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewModel.swift
+//  SwipeMatchFirestore
+//
+//  Created by 小森 将大 on 2019/06/20.
+//  Copyright © 2019 小森 将大. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // we'll define the properties that are view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
+
+
+// what exactly do we do with this card view model thing???
