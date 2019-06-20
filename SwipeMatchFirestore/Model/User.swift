ade5bd421b8cec8474090d22e2a8d5b0b53bc91d
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-06-20 19:32:42 +0900

    finished lesson 6

---
+++ SwipeMatchFirestore/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  SwipeMatchFirestore
+//
+//  Created by 小森 将大 on 2019/06/20.
+//  Copyright © 2019 小森 将大. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    // defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
