3fd5ba3c41253dee5dc3f29f1f51b295c9de7b4a
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-06-30 00:44:58 +0900

    finished lesson 32

---
+++ SwipeMatchFirestore/Controllers/User Details/UserDetailsController.swift
@@ -0,0 +1,19 @@
+//
+//  UserDetailsController.swift
+//  SwipeMatchFirestore
+//
+//  Created by 小森 将大 on 2019/06/30.
+//  Copyright © 2019 小森 将大. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
