6f005d10e264cc1d98e0b1a736062e5243ff81b5
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-07-01 01:28:13 +0900

    finished lesson 36

--- SwipeMatchFirestore/Controllers/User Details/SwipingPhotosController.swift
+++ SwipeMatchFirestore/Controllers/User Details/SwipingPhotosController.swift
@@ -10,20 +10,34 @@ import UIKit
 
 class SwipingPhotosController: UIPageViewController, UIPageViewControllerDataSource {
     
-    let controllers = [
-        PhotoController(image: #imageLiteral(resourceName: "boost_circle")),
-        PhotoController(image: #imageLiteral(resourceName: "refresh_circle")),
-        PhotoController(image: #imageLiteral(resourceName: "like_circle")),
-        PhotoController(image: #imageLiteral(resourceName: "super_like_circle")),
-        PhotoController(image: #imageLiteral(resourceName: "dismiss_circle"))
-    ]
+    var cardViewModel: CardViewModel! {
+        didSet {
+            print(cardViewModel.attributedString)
+            controllers = cardViewModel.imageUrls.map({ (imageUrl) -> UIViewController in
+                let photoController = PhotoController(imageUrl: imageUrl)
+                return photoController
+            })
+            
+            setViewControllers([controllers.first!], direction: .forward, animated: false)
+        }
+    }
+    
+    var controllers = [UIViewController]() // blank array
+    
+//    let controllers = [
+//        PhotoController(image: #imageLiteral(resourceName: "boost_circle")),
+//        PhotoController(image: #imageLiteral(resourceName: "refresh_circle")),
+//        PhotoController(image: #imageLiteral(resourceName: "like_circle")),
+//        PhotoController(image: #imageLiteral(resourceName: "super_like_circle")),
+//        PhotoController(image: #imageLiteral(resourceName: "dismiss_circle"))
+//    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource = self
         view.backgroundColor = .white
         
-        setViewControllers([controllers.first!], direction: .forward, animated: false)
+//        setViewControllers([controllers.first!], direction: .forward, animated: false)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
@@ -44,8 +58,11 @@ class PhotoController: UIViewController {
     
     let imageView = UIImageView(image: #imageLiteral(resourceName: "kelly1"))
     
-    init(image: UIImage) {
-        imageView.image = image
+    // provide an initializer that takes in a URL instead
+    init(imageUrl: String) {
+        if let url = URL(string: imageUrl) {
+            imageView.sd_setImage(with: url)
+        }
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -53,7 +70,7 @@ class PhotoController: UIViewController {
         super.viewDidLoad()
         view.addSubview(imageView)
         imageView.fillSuperview()
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
     }
     
     required init?(coder aDecoder: NSCoder) {
